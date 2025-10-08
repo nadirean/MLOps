37bde01528f6eacdb1ae13abe3d69a5002cae960
nadirean <looxxon@gmail.com>
2025-10-08 09:16:43 +0200

    Modify script to load from secrets.yaml

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import os
 import argparse
 from dotenv import load_dotenv
 from settings import Settings
+import yaml
 
 
 def export_envs(environment: str = "dev") -> None:
+    # Load secrets from secrets.yaml if it exists
+    if os.path.exists("secrets.yaml"):
+        with open("secrets.yaml", "r") as file:
+            secrets = yaml.safe_load(file)
+        for key, value in secrets.items():
+            os.environ[key] = value
+
+    # Load environment variables from the specified .env file
     env_file = f".env.{environment}"
     if os.path.exists(env_file):
         load_dotenv(env_file)
@@ -30,3 +39,4 @@ if __name__ == "__main__":
 
     print("APP_NAME: ", settings.APP_NAME)
     print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("API_KEY: ", settings.API_KEY)
