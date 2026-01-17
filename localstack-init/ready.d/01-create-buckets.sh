556e8b00f7ee668173a81b62272dd9b5a7b549de
nadirean <looxxon@gmail.com>
2026-01-17 14:55:03 +0100

    Modify config for homework

--- localstack-init/ready.d/01-create-buckets.sh
+++ localstack-init/ready.d/01-create-buckets.sh
@@ -2,3 +2,7 @@
 set -e
 
 awslocal s3 mb s3://airflow-xcom || true
+awslocal s3 mb s3://weather-data || true
+awslocal s3 mb s3://nyc-taxi-raw || true
+awslocal s3 mb s3://nyc-taxi-processed || true
+awslocal s3 mb s3://nyc-taxi-models || true
--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -2,3 +2,11 @@ CREATE TABLE exchange_rates (
     symbol VARCHAR(50),
     rate FLOAT
 );
+
+CREATE TABLE IF NOT EXISTS model_performance (
+    id SERIAL PRIMARY KEY,
+    training_date TIMESTAMPTZ NOT NULL,
+    model_name TEXT NOT NULL,
+    train_size INTEGER NOT NULL,
+    test_mae DOUBLE PRECISION NOT NULL
+);
